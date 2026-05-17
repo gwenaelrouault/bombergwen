75b3089fbbc0dfc21e3c4feee67f2cfb056d6b61
gwenael <gwenael.rouault@gmail.com>
2026-05-17 12:10:05 +0200

    init commit main

---
+++ src/desktop.cpp
@@ -0,0 +1,22 @@
+#include <stdexcept>
+#include <iostream>
+#include <string>
+#include <memory>
+#include "game_engine.h"
+#include <syslog.h>
+
+int main(int argc, char **argv)
+{
+    bool result = EXIT_FAILURE;
+    constexpr int SCREEN_WIDTH = 640;
+    constexpr int SCREEN_HEIGHT = 480;
+    BomberLogger logger;
+    if (BomberLogger::get_instance() == nullptr) {
+        return EXIT_FAILURE;
+    }
+    {
+        auto game_engine = make_unique<GameEngine>(SCREEN_WIDTH, SCREEN_HEIGHT);
+        result = game_engine->game_loop();
+    }
+    return result;
+}
